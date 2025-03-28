89c020b6f3ddb2ca83e44134e93b3b04093639a9
Milani Ncana <milzgeorge@live.com>
2025-03-28 14:02:33 +0200

    Completed Entity and Factory class

--- src/main/java/fr/efrei/domain/CommentsEntity.java
+++ src/main/java/fr/efrei/domain/CommentsEntity.java
@@ -2,16 +2,24 @@ package fr.efrei.domain;
 
 public class CommentsEntity {
 
+    private String userID;
     private String userName;
     private String userComments;
+    private int ratings;
 
     public CommentsEntity(){
 
     }
 
     public CommentsEntity(Builder builder) {
+        this.userID = builder.userID;
         this.userName = builder.userName;
         this.userComments = builder.userComments;
+        this.ratings = builder.ratings;
+    }
+
+    public String getUserID(){
+        return userID;
     }
 
     public String getUserName() {
@@ -21,31 +29,48 @@ public class CommentsEntity {
     public String getUserComments() {
         return userComments;
     }
+    public int getRatings(){
+        return ratings;
+    }
 
     @Override
     public String toString() {
-        return "CommentsEntity{" +
+        return "CommentsEntity{" + "userID=" + userID +
                 "userName='" + userName + '\'' +
                 ", userComments='" + userComments + '\'' +
+                ", ratings=" + ratings +
                 '}';
     }
 
-    public static class Builder{
+    public static class Builder {
+        private String userID;
         private String userName;
         private String userComments;
-    }
+        private int ratings;
 
-   /* public Builder setUserName(String userName) {
-        this.userName = userName;
 
-        return this;
-    }
+        public Builder setUserID(String userID) {
+            this.userID = userID;
+            return this;
+        }
 
-    public Builder setUserComments(String userComments) {
-        this.userComments = userComments;
-        return this;
-    }*/
-    public CommentsEntity build(){
-        return new CommentsEntity();
+        public Builder setUserName(String userName) {
+            this.userName = userName;
+            return this;
+        }
+
+        public Builder setUserComments(String userComments) {
+            this.userComments = userComments;
+            return this;
+        }
+
+        public Builder setRatings(int ratings) {
+            this.ratings = ratings;
+            return this;
+        }
+
+        public CommentsEntity build() {
+            return new CommentsEntity(this);
+        }
     }
 }
---
+++ src/main/java/fr/efrei/factory/CommentsFactory.java
@@ -0,0 +1,25 @@
+package fr.efrei.factory;
+
+import fr.efrei.domain.CommentsEntity;
+
+public class CommentsFactory {
+
+    public static CommentsEntity createCommentsEntity(String userID, String userName, String userComments, int ratings) {
+
+        if (userID == null || userID.isEmpty()){
+            return null;
+        }
+        if (userName == null || userName.isEmpty()){
+            return null;
+        }
+        if (userComments == null || userComments.isEmpty()){
+            return null;
+        }
+        if (ratings < 0 || ratings > 5){
+            return null;
+        }
+
+        return new CommentsEntity.Builder().setUserID(userID)
+                .setUserName(userName).setUserComments(userComments).setRatings(ratings).build();
+    }
+}
