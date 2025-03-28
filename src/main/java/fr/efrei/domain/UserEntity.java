5cdfb02d04e4091bb3006045fecb099936f1bd8f
InamJim <222086939@mycput.ac.za>
2025-03-28 11:49:58 +0200

    Done??

---
+++ src/main/java/fr/efrei/domain/UserEntity.java
@@ -0,0 +1,74 @@
+package fr.efrei.domain;
+
+public class UserEntity {
+
+    private String userID;
+    private String userName;
+    private String userEmail;
+    private String userRole;
+
+    private UserEntity(){}
+
+    private UserEntity(Builder builder ){
+        this.userID = builder.userID;
+        this.userName = builder.userName;
+        this.userEmail = builder.userEmail;
+        this.userRole = builder.userRole;
+
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public String getUserRole() {
+        return userRole;
+    }
+
+    @Override
+    public String toString() {
+        return "UserEntity{" +
+                "userID='" + userID + '\'' +
+                ", userName='" + userName + '\'' +
+                ", userEmail='" + userEmail + '\'' +
+                ", userRole='" + userRole + '\'' +
+                '}';
+    }
+
+    public static class Builder{
+        private String userID;
+        private String userName;
+        private String userEmail;
+        private String userRole;
+
+        public Builder setUserID(String userID) {
+            this.userID = userID;
+            return this;
+        }
+
+        public Builder setUserName(String userName) {
+            this.userName = userName;
+            return this;
+        }
+
+        public Builder setUserEmail(String userEmail) {
+            this.userEmail = userEmail;
+            return this;
+        }
+
+        public Builder setUserRole(String userRole) {
+            this.userRole = userRole;
+            return this;
+        }
+
+        public UserEntity build(){ return new UserEntity(this);}
+    }
+}
--- src/main/java/fr/efrei/util/Helper.java
+++ src/main/java/fr/efrei/util/Helper.java
@@ -1,10 +1,22 @@
 package fr.efrei.util;
 
+import org.apache.commons.validator.EmailValidator;
+
 public class Helper {
 
+    public static boolean isValidEmail(String userEmail) {
+        EmailValidator validator = EmailValidator.getInstance();
+        if (validator.isValid(userEmail)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
     public static boolean isNullOrEmpty(String s) {
         if (s.isEmpty() || s == null)
             return true;
+
         return false;
     }
 }
